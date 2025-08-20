d24984972df1f4073560e8c9ecad6e6a4f4d7fe6
hash-shi <hash-shi@tjs-net.co.jp>
2025-08-20 17:31:48 +0900

    必須チェック修正

--- src/jp/co/tjs_net/java/framework/validate/IsRequired.java
+++ src/jp/co/tjs_net/java/framework/validate/IsRequired.java
@@ -3,6 +3,8 @@ package jp.co.tjs_net.java.framework.validate;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+
 import jp.co.tjs_net.java.framework.base.ValidateBase;
 import jp.co.tjs_net.java.framework.information.IndexInformation;
 
@@ -15,6 +17,7 @@ public class IsRequired extends ValidateBase {
 	@Override
 	public boolean doValidate(HttpServletRequest req, HttpServletResponse res, String value, IndexInformation info) {
 		
+		value = StringUtils.defaultString(value);
 		if (value.trim().equals("")){
 			return false;
 		} else {
