675a371ffa4bded677c47daceb8c2c25feafd461
hash-shi <hash-shi@tjs-net.co.jp>
2025-08-21 17:00:22 +0900

    条件付き必須チェックの修正
    targetvalue：条件となる項目
    conditionvalue：条件となる値

--- src/jp/co/tjs_net/java/framework/validate/IsRequiredIf.java
+++ src/jp/co/tjs_net/java/framework/validate/IsRequiredIf.java
@@ -18,12 +18,22 @@ public class IsRequiredIf extends ValidateBase {
 	public boolean doValidate(HttpServletRequest req, HttpServletResponse res, String value, IndexInformation info) {
 		
 		// 特定の項目に値がある場合のみ必須チェックを実施する。
-		String targetvalue = this.params.get("targetvalue").toString();
-		String target	= req.getParameter(targetvalue);
 		
-		if (target.trim().equals("")) {
-			return true;
-		} else {
+		// 条件となる項目
+		String target = null;
+		if (this.params.get("targetvalue") != null) {
+			target = this.params.get("targetvalue").toString();
+			target	= req.getParameter(target).trim();
+		}
+		
+		// 条件となる値
+		String condition = null;
+		if (this.params.get("conditionvalue") != null) {
+			condition = this.params.get("conditionvalue").toString().trim();
+		}
+		
+		// 条件がない場合、通常の必須として扱う
+		if (target == null && condition == null) {
 			value = StringUtils.defaultString(value);
 			if (value.trim().equals("")){
 				return false;
@@ -31,5 +41,31 @@ public class IsRequiredIf extends ValidateBase {
 				return true;
 			}
 		}
+		
+		// 条件が両方ある場合、target = conditionの時だけ必須
+		else if (target != null && condition != null) {
+			if (target.equals(condition)) {
+				value = StringUtils.defaultString(value);
+				if (value.trim().equals("")){
+					return false;
+				} else {
+					return true;
+				}
+			}
+		}
+		
+		// 条件がtargetのみの場合、targetに値がある場合のみ必須
+		else if (target != null && condition == null) {
+			if (!target.equals("")) {
+				value = StringUtils.defaultString(value);
+				if (value.trim().equals("")){
+					return false;
+				} else {
+					return true;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
